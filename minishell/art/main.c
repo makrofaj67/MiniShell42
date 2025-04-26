a71ea43f9b70d0a20ca9be2209819a888debce42
makrofaj67 <dr.ridvanakman@gmail.com>
2025-04-27 00:56:12 +0300

    art

--- minishell/art/main.c
+++ minishell/art/main.c
@@ -6,7 +6,7 @@
 /*   By: rakman <rakman@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 00:32:21 by rakman            #+#    #+#             */
-/*   Updated: 2025/04/27 00:37:47 by rakman           ###   ########.fr       */
+/*   Updated: 2025/04/27 00:55:53 by rakman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 #include <readline/readline.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+
+extern char **environ;
 
 void	clear_screen(char **envp)
 {
@@ -31,17 +33,24 @@ void	clear_screen(char **envp)
 		waitpid(id, NULL, 0);
 }
 
+void	handle_signal(int sig)
+{
+    (void)sig;
+    clear_screen(environ);
+    exit(0);
+}
+
 void	print_prompt(void)
 {
 	char	*user;
 
 	user = getenv("USER");
 	if (user == NULL)
-		printf("\033[38;5;208m╭─user\033[31m is so ");
+		printf("\033[38;5;208m╭─┤ user\033[31m is so ");
 	else
-		printf("\033[38;5;208m╭─%s\033[31m is so ", user);
+		printf("\033[38;5;208m╭─┤ %s\033[31m is so ", user);
 	printf("\033[38;5;208mKoo⅃\033[31m in "
-		"\033[38;5;208mMiniHeLL\n"
+		"\033[38;5;208mMiniHeLL |\n"
 		"\033[38;5;208m╰─λ \033[0m");
 }
 
@@ -54,6 +63,7 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	(void)envp;
 	status = 1;
+	signal(SIGINT, handle_signal);
 	clear_screen(envp);
 	while (status == 1)
 	{
