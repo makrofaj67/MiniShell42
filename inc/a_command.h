1f53c177bdeca97aa50479fb6ecb9ca99eff7ef9
makrofaj67 <dr.ridvanakman@gmail.com>
2025-05-01 11:25:34 +0300

    a_command header

--- inc/a_command.h
+++ inc/a_command.h
@@ -6,15 +6,16 @@
 /*   By: rakman <rakman@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 14:13:44 by rakman            #+#    #+#             */
-/*   Updated: 2025/04/30 21:07:10 by rakman           ###   ########.fr       */
+/*   Updated: 2025/05/01 11:25:22 by rakman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef A_COMMAND_H
 # define A_COMMAND_H
 
-/*
- * Struct and node for multiple line commands
+/**
+** Struct and node for multiple line commands
+** Used to store and manage multi-line input from the user
 */
 typedef struct s_line_node
 {
@@ -29,56 +30,53 @@ typedef struct s_line_list
 	t_line_node	*tail;
 }	t_line_list;
 
-/*
- *	Struct and node for parenthesis and quote
- *	operations
+/**
+** Struct and node for parenthesis tracking
+** Used to maintain a stack of opening parentheses for validation
 */
 typedef struct s_pnode
 {
-	struct s_pnode *prev;
-	struct s_pnode *next;
+	struct s_pnode	*prev;
+	struct s_pnode	*next;
 }	t_pnode;
 
 typedef struct s_pstack
 {
 	int				size;
-	struct s_pnode *head;
-	struct s_pnode *tail;
+	struct s_pnode	*head;
+	struct s_pnode	*tail;
 }	t_pstack;
 
-
-char *get_command(char *prompt);
-/*
- * Error control in command string
+/**
+** Main command processing functions
 */
-int is_command_blank(char *command);
+char	*get_command(char *prompt);
+int		is_command_blank(char *command);
+char	*collect_multiple_line(char *command, char *prompt);
 
-char *collect_multiple_line(char *command, char *prompt);
+/**
+** Command syntax validation functions
+*/
+int		has_unclosed_quotes(char *command);
+int		has_end_backslash(char *command);
+int		parentheses_status(char *command);
 
-/*
- *list utils for multiple commands
- */
-
-void	print_line(t_line_list *line);
-void	free_line_list(t_line_list *list);
-void	add_line_to_line_list(t_line_list *l_list, char *line);;
-t_line_node	*create_node(char *line);
+/**
+** Line list utility functions for multiple commands
+*/
 t_line_list	*init_line(void);
+t_line_node	*create_node(char *line);
+void		add_line_to_line_list(t_line_list *l_list, char *line);
+void		print_line(t_line_list *line);
+void		free_line_list(t_line_list *list);
 
-/*
-* stack utils for parentheses operations
+/**
+** Stack utility functions for parentheses tracking
 */
-
-int parentheses_status(char *command);
-void free_pstack(t_pstack *stack);
-void pop_pstack(t_pstack *stack);
-void push_pstack(t_pstack *stack);
-t_pnode *init_psnode(void);
-t_pstack *init_pstack(void);
-
-//quotes and slashes
-
-int has_unclosed_quotes(char *command);
-int has_end_backslash(char *command);
+t_pstack	*init_pstack(void);
+t_pnode		*init_psnode(void);
+void		push_pstack(t_pstack *stack);
+void		pop_pstack(t_pstack *stack);
+void		free_pstack(t_pstack *stack);
 
 #endif
